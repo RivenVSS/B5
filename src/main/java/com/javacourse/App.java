84a4677d30781ba89944cbefcc75416b1dbaeca6
RivenVSS <cerabn@mail.ru>
2021-04-07 01:36:51 +1000

    Реализовал min/max с помощью массива

--- src/main/java/com/javacourse/App.java
+++ src/main/java/com/javacourse/App.java
@@ -1,8 +1,23 @@
 package com.javacourse;
 
+import com.javacourse.stack.LinkedStack;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		LinkedStack<Integer> stack = new LinkedStack<Integer>();
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+		stack.push(10);
+		stack.push(0);
+		stack.push(2);
+		stack.push(null);
+		stack.push(null);
+		stack.push(null);
+		stack.push(0);
+		System.out.println("Test pop: " + stack.pop());
+		System.out.println("Test peek: " + stack.peek());
+		System.out.println("Max: " + stack.max());
+		System.out.println("Min: " + stack.min());
 	}
 }
